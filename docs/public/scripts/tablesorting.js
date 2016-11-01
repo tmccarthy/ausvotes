e502494d8f58e4bb971ae29a871b0e5ea7b0cab5
tmccarthy <ebh042@gmail.com>
2016-11-01 21:58:24 +1100

    #28: Create project webpage  - Table sorting

--- docs/public/scripts/tablesorting.js
+++ docs/public/scripts/tablesorting.js
@@ -2,16 +2,25 @@ window.onload = function (e) {
   var tables = document.getElementsByTagName('table');
 
   for (var i = 0, table; table = tables[i]; i++) {
-    var lastRow = table.rows[ table.rows.length - 1 ];
+    setupTableForSorting(table)
+  }
+};
 
-    var lastRowIsTotalRow = lastRow.cells[0].innerHTML.includes("Total");
+setupTableForSorting = function(table) {
+  // setSortByValues(table);
+  affixLastRow(table);
 
-    if (lastRowIsTotalRow) {
-      lastRow.classList += " no-sort";
+  new Tablesort(table, {
+    descending: true
+  });
+};
 
-      new Tablesort(table, {
-        descending: true
-      });
-    }
+affixLastRow = function(table) {
+  var lastRow = table.rows[ table.rows.length - 1 ];
+
+  var lastRowIsTotalRow = lastRow.cells[0].innerHTML.includes("Total");
+
+  if (lastRowIsTotalRow) {
+    lastRow.classList += " no-sort";
   }
 };
