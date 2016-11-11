002a06235581973b5f7ad320b380bcab96935751
tmccarthy <ebh042@gmail.com>
2016-11-12 10:52:14 +1100

    #21: Track the transfer value at exhaustion of ballots (still broken)  - Fix performance

--- src/main/scala/au/id/tmm/senatedb/computations/exhaustion/ExhaustionCalculator.scala
+++ src/main/scala/au/id/tmm/senatedb/computations/exhaustion/ExhaustionCalculator.scala
@@ -1,10 +1,12 @@
 package au.id.tmm.senatedb.computations.exhaustion
 
+import au.id.tmm.senatedb.model.CountData
+import au.id.tmm.senatedb.model.CountStep.DistributionStep
 import au.id.tmm.senatedb.model.computation.{BallotExhaustion, NormalisedBallot}
 import au.id.tmm.senatedb.model.parsing.{Ballot, CandidatePosition}
-import au.id.tmm.senatedb.model.{CountData, CountStep}
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object ExhaustionCalculator {
 
@@ -13,9 +15,14 @@ object ExhaustionCalculator {
       case (ballot, normalised) => new TrackedBallot(ballot, normalised)
     }
 
-    countData.distributionSteps.foreach { step =>
-      trackedBallots.foreach(handleBallotForCountStep(step, _))
-    }
+    val ballotsPerAllocation = new mutable.HashMap[Allocation, mutable.Set[TrackedBallot]]
+      with mutable.MultiMap[Allocation, TrackedBallot]
+
+    trackedBallots.toStream
+      .filter(_.currentAllocation.isDefined)
+      .foreach(ballot => ballotsPerAllocation.addBinding(ballot.currentAllocation.get, ballot))
+
+    trackBallotsThroughDistributionSteps(countData.distributionSteps, ballotsPerAllocation)
 
     trackedBallots.toStream
       .map { trackedBallot =>
@@ -24,14 +31,33 @@ object ExhaustionCalculator {
       .toMap
   }
 
-  // TODO the isDistributedBy call is essentially a linear search for ballots distributed at this step.
-  // This is causing a big performance hit
-  private def handleBallotForCountStep(countStep: CountStep.DistributionStep,
-                                       ballot: TrackedBallot): Unit = {
-    if (ballot isDistributedBy countStep) {
-      ballot.transferValue = countStep.source.transferValue
+  @tailrec
+  private def trackBallotsThroughDistributionSteps(distributionSteps: Vector[DistributionStep],
+                                                   ballotsPerAllocation: mutable.MultiMap[Allocation, TrackedBallot]): Unit = {
+    val step = distributionSteps.head
 
-      allocateToNextPreference(ballot, countStep.count, countStep.elected, countStep.excluded)
+    val allocationsReallocatedAtThisStep = step.source.sourceCounts.map(count => Allocation(step.source.sourceCandidate, count))
+
+    val ballotsReallocatedAtThisStep = allocationsReallocatedAtThisStep.flatMap(allocation => {
+      ballotsPerAllocation.getOrElse(allocation, mutable.Set())
+    })
+
+    allocationsReallocatedAtThisStep.foreach(ballotsPerAllocation.remove)
+
+    ballotsReallocatedAtThisStep.foreach(ballot => {
+      ballot.transferValue = step.source.transferValue
+
+      allocateToNextPreference(ballot, step.count, step.elected, step.excluded)
+    })
+
+    ballotsReallocatedAtThisStep.foreach(ballot => {
+      ballot.currentAllocation.foreach(allocation => ballotsPerAllocation.addBinding(allocation, ballot))
+    })
+
+    val remainingSteps = distributionSteps.tail
+
+    if (remainingSteps.nonEmpty) {
+      trackBallotsThroughDistributionSteps(remainingSteps, ballotsPerAllocation)
     }
   }
 
@@ -57,6 +83,8 @@ object ExhaustionCalculator {
     }
   }
 
+  private final case class Allocation(candidatePosition: CandidatePosition, fromCount: Int)
+
   private final class TrackedBallot(val ballot: Ballot,
                                     val normalisedBallot: NormalisedBallot,
 
@@ -69,12 +97,9 @@ object ExhaustionCalculator {
                                     var exhaustedAtCount: Option[Int] = None,
                                     var candidatesElectedAtExhaustion: Option[Int] = None
                                    ) {
-    def currentPreference: Option[CandidatePosition] = normalisedBallot.canonicalOrder.lift(currentPreferenceIndex)
+    def currentAllocation: Option[Allocation] = currentPreference.map(Allocation(_, allocatedAtCount))
 
-    def isDistributedBy(distributionStep: CountStep.DistributionStep): Boolean = {
-      (currentPreference contains distributionStep.source.sourceCandidate) &&
-        (distributionStep.source.sourceCounts contains allocatedAtCount)
-    }
+    def currentPreference: Option[CandidatePosition] = normalisedBallot.canonicalOrder.lift(currentPreferenceIndex)
 
     def toExhaustion: BallotExhaustion = {
       if (isExhausted) {
--- src/test/scala/au/id/tmm/senatedb/fixtures/MockParsedDataStore.scala
+++ src/test/scala/au/id/tmm/senatedb/fixtures/MockParsedDataStore.scala
@@ -1,29 +1,30 @@
 package au.id.tmm.senatedb.fixtures
 
 import au.id.tmm.senatedb.engine.ParsedDataStore
+import au.id.tmm.senatedb.model
 import au.id.tmm.senatedb.model.parsing.Ballot
-import au.id.tmm.senatedb.model.{CountData, DivisionsAndPollingPlaces, GroupsAndCandidates, SenateElection}
+import au.id.tmm.senatedb.model.{CountData, SenateElection}
 import au.id.tmm.senatedb.rawdata.RawDataStore
 import au.id.tmm.utilities.collection.CloseableIterator
 import au.id.tmm.utilities.geo.australia.State
 
 object MockParsedDataStore extends ParsedDataStore {
-  override def groupsAndCandidatesFor(election: SenateElection): GroupsAndCandidates =
+  override def groupsAndCandidatesFor(election: SenateElection): model.GroupsAndCandidates =
     GroupsAndCandidates.ACT.groupsAndCandidates
 
-  override def divisionsAndPollingPlacesFor(election: SenateElection): DivisionsAndPollingPlaces =
+  override def divisionsAndPollingPlacesFor(election: SenateElection): model.DivisionsAndPollingPlaces =
     DivisionsAndPollingPlaces.ACT.divisionsAndPollingPlaces
 
   override def countDataFor(election: SenateElection,
-                            allGroupsAndCandidates: GroupsAndCandidates,
+                            allGroupsAndCandidates: model.GroupsAndCandidates,
                             state: State): CountData = {
     ParsedDataStore(RawDataStore(MockAecResourceStore))
       .countDataFor(election, allGroupsAndCandidates, state)
   }
 
   override def ballotsFor(election: SenateElection,
-                          groupsAndCandidates: GroupsAndCandidates,
-                          divisionsAndPollingPlaces: DivisionsAndPollingPlaces,
+                          groupsAndCandidates: model.GroupsAndCandidates,
+                          divisionsAndPollingPlaces: model.DivisionsAndPollingPlaces,
                           state: State): CloseableIterator[Ballot] = {
     ParsedDataStore(RawDataStore(MockAecResourceStore))
       .ballotsFor(election, groupsAndCandidates, divisionsAndPollingPlaces, state)
