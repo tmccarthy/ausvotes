2e0115882f81618c65a6bee8e4b1da31964f81dc
tmccarthy <ebh042@gmail.com>
2017-08-01 22:16:21 +1000

    #36: Develop a general model for jurisdiction statistics  - Add tests for Tally0

--- core/src/test/scala/au/id/tmm/senatedb/core/tallies/Tally0Spec.scala
+++ core/src/test/scala/au/id/tmm/senatedb/core/tallies/Tally0Spec.scala
@@ -4,8 +4,31 @@ import au.id.tmm.utilities.testing.ImprovedFlatSpec
 
 class Tally0Spec extends ImprovedFlatSpec {
 
-  "a 0 teir tally" can "be summed" in {
+  "a 0 teir tally" can "be added" in {
     assert(Tally0(1) + Tally0(2) === Tally0(3))
+  }
+
+  it can "be divided by a double" in {
+    assert(Tally0(2) / 2 === Tally0(1))
+  }
+
+  it can "be divided by another teir 0 tally" in {
+    assert(Tally0(2) / Tally0(2) === Tally0(1))
+  }
+
+  it should "accumulate to itself" in {
+    assert(Tally0(1).accumulated === Tally0(1))
+  }
+
+  it can "be summed" in {
+    val actual = Tally0.sum(Vector(
+      Tally0(4),
+      Tally0(5),
+      Tally0(6),
+      Tally0(7),
+    ))
+
+    assert(actual === Tally0(4 + 5 + 6 + 7))
   }
 
 }
