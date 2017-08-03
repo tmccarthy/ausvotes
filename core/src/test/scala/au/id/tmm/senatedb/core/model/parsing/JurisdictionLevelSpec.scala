ea0e10759602f3d6c1da7c9ddd827bd2dedbd4d0
tmccarthy <ebh042@gmail.com>
2017-08-03 21:41:25 +1000

    #36: Develop a general model for jurisdiction statistics  - Add tests for JurisdictionLevel

---
+++ core/src/test/scala/au/id/tmm/senatedb/core/model/parsing/JurisdictionLevelSpec.scala
@@ -0,0 +1,25 @@
+package au.id.tmm.senatedb.core.model.parsing
+
+import au.id.tmm.senatedb.core.fixtures.Ballots
+import au.id.tmm.utilities.testing.ImprovedFlatSpec
+
+class JurisdictionLevelSpec extends ImprovedFlatSpec {
+
+  private val testBallot = Ballots.ACT.formalAtl
+
+  "the national jurisdiction level" should "extract the senate election of a ballot" in {
+    assert(JurisdictionLevel.Nation.ofBallot(testBallot) === testBallot.election)
+  }
+
+  "the state jurisdiction level" should "extract the state of a ballot" in {
+    assert(JurisdictionLevel.State.ofBallot(testBallot) === testBallot.state)
+  }
+
+  "the division jurisdiction level" should "extract the division of a ballot" in {
+    assert(JurisdictionLevel.Division.ofBallot(testBallot) === testBallot.division)
+  }
+
+  "the vote collection point jurisdiction level" should "extract the vote collection point of a ballot" in {
+    assert(JurisdictionLevel.VoteCollectionPoint.ofBallot(testBallot) === testBallot.voteCollectionPoint)
+  }
+}
