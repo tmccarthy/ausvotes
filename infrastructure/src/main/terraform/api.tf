6c02f9b38ad412b82974526bb92c03fbac5d4a26
Timothy McCarthy <ebh042@gmail.com>
2018-08-05 16:24:21 +1000

    Recount Lambda

--- infrastructure/src/main/terraform/api.tf
+++ infrastructure/src/main/terraform/api.tf
@@ -11,35 +11,51 @@ resource "aws_api_gateway_deployment" "recount_api_deployment" {
 resource "aws_api_gateway_method_settings" "recount_api_gateway_method_settings" {
   rest_api_id = "${aws_api_gateway_rest_api.recount_api.id}"
   stage_name  = "${aws_api_gateway_deployment.recount_api_deployment.stage_name}"
-  method_path = "${aws_api_gateway_resource.resource.path_part}/${aws_api_gateway_method.method.http_method}"
+  method_path = "${aws_api_gateway_resource.recount_resource.path_part}/${aws_api_gateway_method.recount_election_state_resource_method.http_method}"
 
   settings {
     metrics_enabled = false
   }
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part = "display"
+resource "aws_api_gateway_resource" "recount_resource" {
+  path_part = "recount"
   parent_id = "${aws_api_gateway_rest_api.recount_api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.recount_api.id}"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_resource" "recount_election_resource" {
+  path_part = "{election}"
+  parent_id = "${aws_api_gateway_resource.recount_resource.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.recount_api.id}"
+}
+
+resource "aws_api_gateway_resource" "recount_election_state_resource" {
+  path_part = "{state}"
+  parent_id = "${aws_api_gateway_resource.recount_election_resource.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.recount_api.id}"
+}
+
+resource "aws_api_gateway_method" "recount_election_state_resource_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.recount_api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  resource_id   = "${aws_api_gateway_resource.recount_election_state_resource.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.recount_api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
+  resource_id             = "${aws_api_gateway_resource.recount_election_state_resource.id}"
+  http_method             = "${aws_api_gateway_method.recount_election_state_resource_method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2018-07-28/functions/${aws_lambda_function.recount.arn}/invocations"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.recount.arn}/invocations"
 
   request_templates {
-    "application/json" = ""
+    "application/json" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
   }
 }
--- infrastructure/src/main/terraform/lambdas.tf
+++ infrastructure/src/main/terraform/lambdas.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "recount" {
   filename         = "${var.recount_lambda_zip_path}"
   function_name    = "recount"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "au.id.tmm.ausvotes.lambda.recount.RecountLambda"
+  handler          = "au.id.tmm.ausvotes.lambdas.recount.Lambda"
   source_code_hash = "${base64sha256(file(var.recount_lambda_zip_path))}"
   runtime          = "java8"
   timeout          = 30
