7adb6dc4165b72d5095ad553d94954e4b15cae9b
Timothy McCarthy <ebh042@gmail.com>
2018-08-11 14:24:50 +1000

    Move fixtures into their own Gradle module

--- core/src/test/scala/au/id/tmm/ausvotes/core/rawdata/download/DownloadUtilsSpec.scala
+++ core/src/test/scala/au/id/tmm/ausvotes/core/rawdata/download/DownloadUtilsSpec.scala
@@ -8,7 +8,7 @@ import au.id.tmm.utilities.testing.ImprovedFlatSpec
 class DownloadUtilsSpec extends ImprovedFlatSpec {
 
   "the local resources check" should "fail if the digest doesn't match" in {
-    val file = Paths.get(getClass.getResource("/au/id/tmm/ausvotes/core/fixtures/firstPreferencesTest.csv").toURI)
+    val file = Paths.get(getClass.getResource("/au/id/tmm/ausvotes/core/rawdata/download/test_resource.txt").toURI)
     val badDigest = Digest("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
     intercept[DataIntegrityException] {
