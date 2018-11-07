10888a7f48fdcf753afa6dad8b19886df86ab491
Timothy McCarthy <ebh042@gmail.com>
2018-11-07 21:22:28 +1100

    Move recount resources into shared directory

---
+++ shared/recountResources/src/main/scala/au/id/tmm/ausvotes/shared/recountresources/RecountResponse.scala
@@ -0,0 +1,48 @@
+package au.id.tmm.ausvotes.shared.recountresources
+
+import argonaut.Argonaut._
+import argonaut.EncodeJson
+import au.id.tmm.ausvotes.core.model.codecs.CandidateCodec.encodeCandidate
+import au.id.tmm.ausvotes.core.model.codecs.PartyCodec.encodeParty
+
+sealed trait RecountResponse
+
+object RecountResponse {
+
+  sealed trait Failure extends RecountResponse
+
+  object Failure {
+    final case class RequestDecodeError(message: String, request: String) extends Failure
+    final case class InvalidCandidateIds(invalidCandidateAecIds: Set[String]) extends Failure
+    case object InternalError extends Failure
+  }
+
+  final case class Success(recountResult: RecountResult) extends RecountResponse
+
+  implicit val encodeRecountResponse: EncodeJson[RecountResponse] = {
+    case Success(recountResult) =>
+      jObjectFields(
+        "success" -> true.asJson,
+        "recountResult" -> recountResult.asJson,
+      )
+    case Failure.RequestDecodeError(message, request) =>
+      jObjectFields(
+        "success" -> false.asJson,
+        "errorType" -> "RequestDecodeError".asJson,
+        "message" -> message.asJson,
+        "request" -> request.asJson,
+      )
+    case Failure.InvalidCandidateIds(invalidCandidateAecIds) =>
+      jObjectFields(
+        "success" -> false.asJson,
+        "errorType" -> "InvalidCandidateIds".asJson,
+        "invalidCandidateIds" -> invalidCandidateAecIds.asJson,
+      )
+    case Failure.InternalError =>
+      jObjectFields(
+        "success" -> false.asJson,
+        "errorType" -> "InternalError".asJson,
+      )
+  }
+
+}
---
+++ shared/recountResources/src/test/scala/au/id/tmm/ausvotes/shared/recountresources/RecountResponseSpec.scala
@@ -0,0 +1,62 @@
+package au.id.tmm.ausvotes.shared.recountresources
+
+import argonaut.Argonaut._
+import au.id.tmm.ausvotes.core.model.codecs.CandidateCodec.encodeCandidate
+import au.id.tmm.ausvotes.core.model.codecs.PartyCodec.encodeParty
+import au.id.tmm.utilities.testing.ImprovedFlatSpec
+
+class RecountResponseSpec extends ImprovedFlatSpec {
+
+  "the recount response encoder" can "encode a failure due to a bad request" in {
+    val failure: RecountResponse = RecountResponse.Failure.RequestDecodeError("Invalid json", "{")
+
+    val expectedJson = jObjectFields(
+      "success" -> false.asJson,
+      "errorType" -> "RequestDecodeError".asJson,
+      "message" -> "Invalid json".asJson,
+      "request" -> "{".asJson,
+    )
+
+    assert(failure.asJson === expectedJson)
+  }
+
+  it can "encode a failure due to invalid candidate ids" in {
+    val failure: RecountResponse = RecountResponse.Failure.InvalidCandidateIds(Set("123", "456"))
+
+    val expectedJson = jObjectFields(
+      "success" -> false.asJson,
+      "errorType" -> "InvalidCandidateIds".asJson,
+      "invalidCandidateIds" -> jArrayElements(
+        "123".asJson,
+        "456".asJson,
+      ),
+    )
+
+    assert(failure.asJson === expectedJson)
+  }
+
+  it can "encode an internal error" in {
+    val failure: RecountResponse = RecountResponse.Failure.InternalError
+
+    val expectedJson = jObjectFields(
+      "success" -> false.asJson,
+      "errorType" -> "InternalError".asJson,
+    )
+
+    assert(failure.asJson === expectedJson)
+  }
+
+  it can "encode a success" in {
+    val recountResponse: RecountResponse = RecountResponse.Success(
+      RecountResultSpec.recountResultFixture
+    )
+
+    val expectedJson = jObjectFields(
+      "success" -> true.asJson,
+      "recountResult" -> RecountResultSpec.recountResultFixture.asJson,
+    )
+
+    assert(recountResponse.asJson === expectedJson)
+  }
+
+}
