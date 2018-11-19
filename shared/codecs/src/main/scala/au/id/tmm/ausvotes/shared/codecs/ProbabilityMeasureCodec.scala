869bf66d08655efac1b7f077b436a6e6d662f16b
Timothy McCarthy <ebh042@gmail.com>
2018-11-19 22:31:49 +1100

    Add codec for ProbabilityMeasure

---
+++ shared/codecs/src/main/scala/au/id/tmm/ausvotes/shared/codecs/ProbabilityMeasureCodec.scala
@@ -0,0 +1,42 @@
+package au.id.tmm.ausvotes.shared.codecs
+
+import argonaut.Argonaut._
+import argonaut.{DecodeJson, DecodeResult, EncodeJson}
+import au.id.tmm.utilities.probabilities.ProbabilityMeasure
+import spire.math.Rational
+
+object ProbabilityMeasureCodec {
+
+  import RationalCodec._
+
+  implicit def encodeProbabilityMeasure[A : EncodeJson]: EncodeJson[ProbabilityMeasure[A]] =
+    EncodeJson { probabilityMeasure =>
+      jArrayElements(
+        probabilityMeasure.asMap.toList.map { case (possibility, probability) =>
+          jObjectFields(
+            "probability" -> probability.asJson,
+            "outcome" -> possibility.asJson,
+          )
+        }: _*
+      )
+    }
+
+  implicit def decodeProbabilityMeasure[A : DecodeJson]: DecodeJson[ProbabilityMeasure[A]] = DecodeJson { c =>
+    for {
+      elements <- c.as[List[(A, Rational)]] (ListDecodeJson(decodeProbabilityMeasureElement))
+      asMap = elements.toMap
+      probabilityMeasure <- ProbabilityMeasure(asMap) match {
+        case Right(probabilityMeasure) => DecodeResult.ok(probabilityMeasure)
+        case Left(constructionError) => DecodeResult.fail(constructionError.toString, c.history)
+      }
+    } yield probabilityMeasure
+  }
+
+  private def decodeProbabilityMeasureElement[A : DecodeJson]: DecodeJson[(A, Rational)] = DecodeJson { c =>
+    for {
+      probability <- c.downField("probability").as[Rational]
+      outcome <- c.downField("outcome").as[A]
+    } yield outcome -> probability
+  }
+
+}
---
+++ shared/codecs/src/test/scala/au/id/tmm/ausvotes/shared/codecs/ProbabilityMeasureCodecSpec.scala
@@ -0,0 +1,64 @@
+package au.id.tmm.ausvotes.shared.codecs
+
+import argonaut.Argonaut._
+import argonaut.DecodeResult
+import au.id.tmm.utilities.probabilities.ProbabilityMeasure
+import au.id.tmm.utilities.testing.ImprovedFlatSpec
+import spire.math.Rational
+
+class ProbabilityMeasureCodecSpec extends ImprovedFlatSpec {
+
+  import ProbabilityMeasureCodec._
+
+  "The probability measure codec" can "encode a probability measure" in {
+    val probabilityMeasure = ProbabilityMeasure(
+      "hello" -> Rational(1, 3),
+      "world" -> Rational(2, 3),
+    ).getOrElse(fail())
+
+    val expectedJson = jArrayElements(
+      jObjectFields(
+        "probability" -> "2/3".asJson,
+        "outcome" -> "world".asJson,
+      ),
+      jObjectFields(
+        "probability" -> "1/3".asJson,
+        "outcome" -> "hello".asJson,
+      ),
+    )
+
+    assert(probabilityMeasure.asJson === expectedJson)
+  }
+
+  it can "decode a probability measure" in {
+    val json = jArrayElements(
+      jObjectFields(
+        "probability" -> "2/3".asJson,
+        "outcome" -> "world".asJson,
+      ),
+      jObjectFields(
+        "probability" -> "1/3".asJson,
+        "outcome" -> "hello".asJson,
+      ),
+    )
+
+    val expectedProbabilityMeasure = ProbabilityMeasure(
+      "hello" -> Rational(1, 3),
+      "world" -> Rational(2, 3),
+    ).getOrElse(fail())
+
+    assert(json.as[ProbabilityMeasure[String]] === DecodeResult.ok(expectedProbabilityMeasure))
+  }
+
+  it should "fail to decode an invalid probability measure" in {
+    val json = jArrayElements(
+      jObjectFields(
+        "probability" -> "2/3".asJson,
+        "outcome" -> "world".asJson,
+      ),
+    )
+
+    assert(json.as[ProbabilityMeasure[String]].message === Some("ProbabilitiesDontSumToOne"))
+  }
+
+}
