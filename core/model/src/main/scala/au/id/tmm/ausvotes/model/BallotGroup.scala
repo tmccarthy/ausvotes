c3a8ac1496681e5bd9c6a69b3327efd7d17ecb0c
Timothy McCarthy <ebh042@gmail.com>
2018-12-25 23:32:30 +1030

    Json encoders and decoders for groups

--- core/model/src/main/scala/au/id/tmm/ausvotes/model/BallotGroup.scala
+++ core/model/src/main/scala/au/id/tmm/ausvotes/model/BallotGroup.scala
@@ -1,10 +1,74 @@
 package au.id.tmm.ausvotes.model
 
 import au.id.tmm.ausvotes.model.BallotGroup.Code
+import au.id.tmm.ausvotes.model.Codecs.Codec
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, DecodingFailure, Encoder, Json}
 
 sealed trait BallotGroup[E] {
   def election: E
   def code: BallotGroup.Code
+}
+
+object BallotGroup {
+
+  implicit def encoder[E : Encoder]: Encoder[BallotGroup[E]] = { group =>
+    val baseJson = Json.obj(
+      "election" -> group.election.asJson,
+      "code" -> group.code.asJson,
+    )
+
+    group match {
+      case Group(_, _, party) => baseJson.deepMerge(Json.obj("party" -> party.asJson))
+      case Ungrouped(_) => baseJson
+    }
+  }
+
+  implicit def decoder[E : Decoder]: Decoder[BallotGroup[E]] = c =>
+    for {
+      election <- c.get[E]("election")
+      code <- c.get[Code]("code")
+      party <- c.get[Option[Party]]("party")
+      ballotGroup <- code match {
+        case Ungrouped.code => Right(Ungrouped(election))
+        case code => Group(election, code, party).left.map(_ => DecodingFailure("Invalid group code", c.history))
+      }
+    } yield ballotGroup
+
+  final case class Code private (asString: String) extends AnyVal {
+    def index: Int = {
+      def charValue(char: Char) = char.toUpper - 'A'
+
+      if (asString.length == 1) {
+        charValue(asString.charAt(0))
+      } else if (asString == "UG") {
+        Int.MaxValue
+      } else {
+        (26 * (1 + charValue(asString.charAt(0)))) + charValue(asString.charAt(1))
+      }
+    }
+  }
+
+  object Code {
+
+    private val validCodePattern = "^[A-Z]{1,2}$".r
+
+    def apply(asString: String): Either[InvalidCode, Code] = asString match {
+      case validCodePattern() => Right(new Code(asString))
+      case invalidCode => Left(InvalidCode(invalidCode))
+    }
+
+    private[model] def unsafeMake(asString: String): Code = Code(asString).right.get
+
+    final case class InvalidCode(invalidCode: String) extends ExceptionCaseClass
+
+    implicit val codec: Codec[Code] = Codecs.partialCodec[Code, String](
+      encode = _.asString,
+      decode = Code(_).toOption,
+    )
+
+  }
+
 }
 
 final case class Group[E] private (
@@ -36,36 +100,4 @@ final case class Ungrouped[E](
 
 object Ungrouped {
   val code: BallotGroup.Code = Code.unsafeMake("UG")
-}
-
-object BallotGroup {
-
-  final case class Code private (asString: String) extends AnyVal {
-    def index: Int = {
-      def charValue(char: Char) = char.toUpper - 'A'
-
-      if (asString.length == 1) {
-        charValue(asString.charAt(0))
-      } else if (asString == "UG") {
-        Int.MaxValue
-      } else {
-        (26 * (1 + charValue(asString.charAt(0)))) + charValue(asString.charAt(1))
-      }
-    }
-  }
-
-  object Code {
-
-    private val validCodePattern = "^[A-Z]{1,2}$".r
-
-    def apply(asString: String): Either[InvalidCode, Code] = asString match {
-      case validCodePattern() => Right(new Code(asString))
-      case invalidCode => Left(InvalidCode(invalidCode))
-    }
-
-    private[model] def unsafeMake(asString: String): Code = Code(asString).right.get
-
-    final case class InvalidCode(invalidCode: String) extends ExceptionCaseClass
-  }
-
 }
--- core/model/src/test/scala/au/id/tmm/ausvotes/model/BallotGroupSpec.scala
+++ core/model/src/test/scala/au/id/tmm/ausvotes/model/BallotGroupSpec.scala
@@ -2,11 +2,45 @@ package au.id.tmm.ausvotes.model
 
 import au.id.tmm.ausvotes.model.federal.SenateElection
 import au.id.tmm.utilities.testing.ImprovedFlatSpec
+import io.circe.Json
+import io.circe.syntax.EncoderOps
 
 class BallotGroupSpec extends ImprovedFlatSpec {
 
   "a grouped ballot group" can "not have a code of UG" in {
     assert(Group(SenateElection.`2016`, Ungrouped.code, party = None) === Left(Group.InvalidGroupCode))
+  }
+
+  it can "be encoded to json" in {
+    val group: BallotGroup[SenateElection] = Group[SenateElection](
+      SenateElection.`2016`,
+      BallotGroup.Code.unsafeMake("A"),
+      Some(Party("Australian Labor Party")),
+    ).right.get
+
+    val json = Json.obj(
+      "election" -> group.election.asJson,
+      "code" -> group.code.asJson,
+      "party" -> Party("Australian Labor Party").asJson,
+    )
+
+    assert(group.asJson === json)
+  }
+
+  it can "be decoded from json" in {
+    val group: BallotGroup[SenateElection] = Group[SenateElection](
+      SenateElection.`2016`,
+      BallotGroup.Code.unsafeMake("A"),
+      Some(Party("Australian Labor Party")),
+    ).right.get
+
+    val json = Json.obj(
+      "election" -> group.election.asJson,
+      "code" -> group.code.asJson,
+      "party" -> Party("Australian Labor Party").asJson,
+    )
+
+    assert(json.as[BallotGroup[SenateElection]] === Right(group))
   }
 
   "an ungrouped ballot group" should "have a code of UG" in {
