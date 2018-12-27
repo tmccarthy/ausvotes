1e0f92a4d8d2894a80edc0f26a93dd13b551492c
Timothy McCarthy <ebh042@gmail.com>
2018-12-27 14:55:13 +1030

    Add flyweights for electorates and groups

---
+++ core/model/src/main/scala/au/id/tmm/ausvotes/model/Flyweights.scala
@@ -0,0 +1,28 @@
+package au.id.tmm.ausvotes.model
+
+import au.id.tmm.ausvotes.model.stv.{BallotGroup, Group}
+import au.id.tmm.utilities.collection.Flyweight
+
+object Flyweights {
+
+  final class ElectorateFlyweight[E] private () {
+    private val underlying: Flyweight[(E, String, Electorate.Id), Electorate[E]] = Flyweight {
+      case (election, name, id) => Electorate(election, name, id)
+    }
+  }
+
+  object ElectorateFlyweight {
+    def apply[E](): ElectorateFlyweight[E] = new ElectorateFlyweight()
+  }
+
+  final class GroupFlyweight[E] private () {
+    private val underlying: Flyweight[(E, BallotGroup.Code, Option[Party]), Either[Group.InvalidGroupCode.type, Group[E]]] = Flyweight {
+      case (election, code, party) => Group(election, code, party)
+    }
+  }
+
+  object GroupFlyweight {
+    def apply[E](): GroupFlyweight[E] = new GroupFlyweight()
+  }
+
+}
