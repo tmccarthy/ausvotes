c6de4d6195160eb47c5c81051531416d4146ed96
Timothy McCarthy <ebh042@gmail.com>
2019-06-02 11:53:20 +1000

    Delete Federal VCP jurisdiction

--- core/model/src/main/scala/au/id/tmm/ausvotes/model/federal/FederalVcpJurisdiction.scala
+++
@@ -1,8 +0,0 @@
-package au.id.tmm.ausvotes.model.federal
-
-import au.id.tmm.utilities.geo.australia.State
-
-final case class FederalVcpJurisdiction(
-                                         state: State,
-                                         division: Division,
-                                       )
